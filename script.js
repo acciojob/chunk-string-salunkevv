e0c973f1639cb842de455fba4a633e7615d43ac6
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-18 12:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function stringChop(str, size) {
+function stringChop(str,length) {
   // your code here
+	let ans=str.match(/.{1,n}/g);
+	// for(let i=0;i<str.length;i=i+size)
+	// 	{
+	// 		let n=i+size;
+	// 		if(n>str.length)
+	// 		{
+	// 			n=str.length+1;
+	// 		}
+	// 		ans.push(str.slice(i,n));
+	// 	}
+	return str.match(new RegExp('.{1,' + length + '}', 'g'));;
 }
 
 // Do not change the code below
