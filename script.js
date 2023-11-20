d92fb20db4b2370449d73b73911d4eefa288b30d
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-20 14:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,30 +2,31 @@ function stringChop(str,size) {
   // your code here
 	let i=0;
 	let ans=[];
-	while(i<=str.length-1)
-		{
-			ans.push(str.slice(i,i+size))
-			i=i+size;
-		}
-	if(str.length ==0)
+	if(str === null)
 	{
 		return ans;
 	}
-	//let ans=str.match(/.{1,n}/g);
-	// for(let i=0;i<str.length;i=i+size)
+	// while(i<=str.length-1)
 	// 	{
-	// 		let n=i+size;
-	// 		if(n>str.length)
-	// 		{
-	// 			n=str.length+1;
-	// 		}
-	// 		ans.push(str.slice(i,n));
+	// 		ans.push(str.slice(i,i+size))
+	// 		i=i+size;
 	// 	}
+	
+	//let ans=str.match(/.{1,n}/g);
+	for(let i=0;i<str.length;i=i+size)
+		{
+			let n=i+size;
+			if(n>str.length)
+			{
+				n=str.length+1;
+			}
+			ans.push(str.slice(i,n));
+		}
 	//return str.match(new RegExp('.{1,' + length + '}', 'g'));;
 	return ans;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
